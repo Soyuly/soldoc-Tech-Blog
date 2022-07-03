f9068b763c57d28fbde7455e9faec411917047dc
Soyuly <soyul8147@gmail.com>
2022-07-03 12:14:37 +0900

    Initial commit

---
+++ src/test/java/com/soldoc/tech/SoldocBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soldoc.tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoldocBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
