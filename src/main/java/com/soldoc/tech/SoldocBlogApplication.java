d4d4ff82d6b3212b70c1cb8560c20b1e726d8ecf
Soyuly <soyul8147@gmail.com>
2022-07-05 10:37:57 +0900

    OAuth 2.0 & Jwt

--- src/main/java/com/soldoc/tech/SoldocBlogApplication.java
+++ src/main/java/com/soldoc/tech/SoldocBlogApplication.java
@@ -1,7 +1,9 @@
 package com.soldoc.tech;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 public class SoldocBlogApplication {
